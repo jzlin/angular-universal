523ea658950c28c94528e6ae761a5d9565db4cef
Jerry Lin <jerry.jzlin@gmail.com>
2017-06-05 01:02:35 +0800

    Server Transition. Configuration - AOT.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,9 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({
+      appId: 'toh-universal'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
